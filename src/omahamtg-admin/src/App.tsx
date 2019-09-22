8e886f7b906bb3c01e08339643e596cbb9ae9694
Brian Olson <beolson@hero4hire.com>
2019-09-22 10:18:36 -0500

    Add react router

--- src/omahamtg-admin/src/App.tsx
+++ src/omahamtg-admin/src/App.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import styles from './App.module.scss';
 import Sponsors from './screens/Sponsors';
-// import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
 // import { CounterButton } from './components/CounterButton';
 
 const App = () => {
   return (
-    <div className={styles.app}>
-      <div className={styles.head}>OmahaMTG Admin</div>
-      <div className={styles.body}>
-        <div className={styles.sidebar}>
-          <ul>
-            <li>Sponsors</li>
-            <li>Hosts</li>
-            <li>Posts</li>
-            <li>Events</li>
-            <li>Presenters</li>
-          </ul>
-        </div>
-        <div className={styles.content}>
-          <Sponsors />
+    <Router>
+      <div className={styles.app}>
+        <div className={styles.head}>OmahaMTG Admin</div>
+        <div className={styles.body}>
+          <div className={styles.sidebar}>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Sponsors/">Sponsors</Link>
+              </li>
+              <li>
+                <Link to="/Hosts/">Hosts</Link>
+              </li>
+              <li>
+                <Link to="/Posts/">Posts</Link>
+              </li>
+              <li>
+                <Link to="/Events/">Events</Link>
+              </li>
+              <li>
+                <Link to="/Presenters/">Presenters</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.content}>
+            <Route path="/Sponsors/" component={Sponsors} />
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
