b65ec964a1efb50e71acaf6968f338c61c868680
Brian Olson <beolson@hero4hire.com>
2019-10-14 20:51:10 -0500

    setup migration

--- CreateOmahaMtgMigration.bat
+++ CreateOmahaMtgMigration.bat
@@ -1,4 +1,4 @@
 echo off
 set migrationName=%1
 
-dotnet ef migrations add --project .\src\OmahaMTG\ --startup-project .\src\OmahaMTG.Api\ --context UserGroupContext %migrationName%
+dotnet ef migrations add --project .\src\OmahaMTG\ --startup-project .\src\OmahaMTG.Site\ --context UserGroupContext %migrationName%
---
+++ src/OmahaMTG/Migrations/20191015015033_AddTemplate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OmahaMTG.Migrations
+{
+    public partial class AddTemplate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactInfo",
+                table: "Presenters",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Templates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Templates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "ContactInfo",
+                table: "Presenters");
+        }
+    }
+}
