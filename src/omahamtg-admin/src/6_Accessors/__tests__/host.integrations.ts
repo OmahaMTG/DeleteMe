c764464500bee7f75dd5f4ccea3b012a7f2fe96e
Brian Olson <beolson@hero4hire.com>
2019-11-20 07:43:33 -0600

    update

--- src/omahamtg-admin/src/6_Accessors/__tests__/host.integrations.ts
+++ src/omahamtg-admin/src/6_Accessors/__tests__/host.integrations.ts
@@ -19,31 +19,57 @@ describe('host API', () => {
     testDataManager.CleanAllResources();
   });
 
-  it('Creating a host, should create that host.', async () => {
-    await testDataManager.CreateTestResources();
+  // it('Creating a host, should create that host.', async () => {
+  //   await testDataManager.CreateTestResources();
 
-    const createdResponse = testDataManager.currentResources[0].created;
-    const requestedResource = testDataManager.currentResources[0].requested;
+  //   const createdResponse = testDataManager.currentResources[0].created;
+  //   const requestedResource = testDataManager.currentResources[0].requested;
 
-    expect(createdResponse.address).toEqual(requestedResource.address);
-    expect(createdResponse.blurb).toEqual(requestedResource.blurb);
-    expect(createdResponse.contactInfo).toEqual(requestedResource.contactInfo);
-    expect(createdResponse.name).toEqual(requestedResource.name);
+  //   expect(createdResponse.address).toEqual(requestedResource.address);
+  //   expect(createdResponse.blurb).toEqual(requestedResource.blurb);
+  //   expect(createdResponse.contactInfo).toEqual(requestedResource.contactInfo);
+  //   expect(createdResponse.name).toEqual(requestedResource.name);
 
-    const hostFromServer = await hostApi.getResource(createdResponse.id);
+  //   const hostFromServer = await hostApi.getResource(createdResponse.id);
+
+  //   expect(hostFromServer).not.toBeUndefined();
+
+  //   expect(hostFromServer && hostFromServer.address).toEqual(requestedResource.address);
+  //   expect(hostFromServer && hostFromServer.blurb).toEqual(requestedResource.blurb);
+  //   expect(hostFromServer && hostFromServer.contactInfo).toEqual(requestedResource.contactInfo);
+  //   expect(hostFromServer && hostFromServer.name).toEqual(requestedResource.name);
+  // });
+
+  it('Creating a host, should create that host and return a record of that host.', async () => {
+    const createdIndex = await testDataManager.CreateTestResources();
+
+    const requestedResource = testDataManager.currentResources[createdIndex].requested;
+    const createdResource = testDataManager.currentResources[createdIndex].created;
+    const hostFromServer = await hostApi.getResource(createdResource.id);
 
     expect(hostFromServer).not.toBeUndefined();
-
     expect(hostFromServer && hostFromServer.address).toEqual(requestedResource.address);
     expect(hostFromServer && hostFromServer.blurb).toEqual(requestedResource.blurb);
     expect(hostFromServer && hostFromServer.contactInfo).toEqual(requestedResource.contactInfo);
     expect(hostFromServer && hostFromServer.name).toEqual(requestedResource.name);
   });
 
-  it('Deleting a host, should delete that host', async () => {
-    await testDataManager.CreateTestResources(1);
+  it('Getting a host, should return that host.', async () => {
+    const createdIndex = await testDataManager.CreateTestResources();
 
-    const createdResponse = testDataManager.currentResources[0].created;
+    const createdResponse = testDataManager.currentResources[createdIndex].created;
+    const requestedResource = testDataManager.currentResources[createdIndex].requested;
+
+    expect(createdResponse.address).toEqual(requestedResource.address);
+    expect(createdResponse.blurb).toEqual(requestedResource.blurb);
+    expect(createdResponse.contactInfo).toEqual(requestedResource.contactInfo);
+    expect(createdResponse.name).toEqual(requestedResource.name);
+  });
+
+  it('Deleting a host, should delete that host', async () => {
+    const createdIndex = await testDataManager.CreateTestResources();
+
+    const createdResponse = testDataManager.currentResources[createdIndex].created;
 
     await hostApi.deleteResource(createdResponse.id, true);
 
@@ -52,27 +78,27 @@ describe('host API', () => {
     expect(hostFromServer).toBeUndefined();
   });
 
-  it('Updating a host, should update that host.', async () => {
-    await testDataManager.CreateTestResources(1);
+  // it('Updating a host, should update that host.', async () => {
+  //   await testDataManager.CreateTestResources(1);
 
-    const createdResponse = testDataManager.currentResources[0].created;
+  //   const createdResponse = testDataManager.currentResources[0].created;
 
-    const updatedResponse = await testDataManager.UpdateResource(createdResponse.id);
+  //   const updatedResponse = await testDataManager.UpdateResource(createdResponse.id);
 
-    const requestedUpdate = testDataManager.currentResources[0].requested;
+  //   const requestedUpdate = testDataManager.currentResources[0].requested;
 
-    expect(updatedResponse.address).toEqual(requestedUpdate.address);
-    expect(updatedResponse.blurb).toEqual(requestedUpdate.blurb);
-    expect(updatedResponse.contactInfo).toEqual(requestedUpdate.contactInfo);
-    expect(updatedResponse.name).toEqual(requestedUpdate.name);
+  //   expect(updatedResponse.address).toEqual(requestedUpdate.address);
+  //   expect(updatedResponse.blurb).toEqual(requestedUpdate.blurb);
+  //   expect(updatedResponse.contactInfo).toEqual(requestedUpdate.contactInfo);
+  //   expect(updatedResponse.name).toEqual(requestedUpdate.name);
 
-    const hostFromServer = await hostApi.getResource(createdResponse.id);
+  //   const hostFromServer = await hostApi.getResource(createdResponse.id);
 
-    expect(hostFromServer).not.toBeUndefined();
+  //   expect(hostFromServer).not.toBeUndefined();
 
-    expect(hostFromServer && hostFromServer.address).toEqual(requestedUpdate.address);
-    expect(hostFromServer && hostFromServer.blurb).toEqual(requestedUpdate.blurb);
-    expect(hostFromServer && hostFromServer.contactInfo).toEqual(requestedUpdate.contactInfo);
-    expect(hostFromServer && hostFromServer.name).toEqual(requestedUpdate.name);
-  });
+  //   expect(hostFromServer && hostFromServer.address).toEqual(requestedUpdate.address);
+  //   expect(hostFromServer && hostFromServer.blurb).toEqual(requestedUpdate.blurb);
+  //   expect(hostFromServer && hostFromServer.contactInfo).toEqual(requestedUpdate.contactInfo);
+  //   expect(hostFromServer && hostFromServer.name).toEqual(requestedUpdate.name);
+  // });
 });
